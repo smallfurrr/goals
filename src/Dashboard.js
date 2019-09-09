eda7525d740c7b9d52a3f9f2755b027ac1fb23c0
EliseChia <swedesurrender@gmail.com>
2019-09-09 13:09:49 +0800

    shows number of overdue goals

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -10,7 +10,7 @@ Text } from 'grommet';
 export default class extends Component {
 
     constructor(props) {
-        super(props)
+        super(props);
 
         this.state = {
             goals: [],
@@ -18,38 +18,45 @@ export default class extends Component {
     }
 
     componentDidMount() {
-
         this.setState({
             goals: ls.get('goals') || [],
         })
     }
 
+    getActiveGoals = goals => {
+        const activeGoals = goals.length;
+        return activeGoals;
+    }
+
+    getOverdueGoals = goals => {
+        let overDue = [];
+
+        for (let i=0; i < goals.length; i++) {
+
+            let endDate = goals[i].endDate;
+
+            let endDateMoment = moment(endDate, 'YYYY-MM-DD').toDate();
+
+            let difference = (moment(endDateMoment).fromNow());
+
+            console.log(difference);
+
+            if (difference.includes("ago")) {
+                overDue.push("orhor")
+            }
+        }
+
+        return overDue.length;
+    }
+
     render() {
-
-    // const goals = ls.get('goals')
-
-    // for (let i=0; i < goals.length; i++) {
-    //     let endDate = goals[i].endDate;
-    //     let holyfuck = moment(endDate, 'YYYY-MM-DD').toDate();
-    //     console.log(holyfuck)
-    //     console.log(typeof holyfuck);
-    //     console.log(moment(holyfuck).fromNow())
-    // }
 
     //declare an empty overdue array
     //loop through the goals array and get each endDate
     //compare each endDate to current moment
-    //if its a ??? value then push it into the overdue array
+    //if its a ??? value (must see how to check leh maybe see if string includes "fron" or "to" or can i just direct compare the dates using >) then push it into the overdue array
     //get length of the overdue array
 
-    // const fuck = goals.map((goals, index) => {
-
-    //         return (
-    //         <Box key={index}>
-    //             <Text>{goals.endDate}</Text>
-    //         </Box>
-    //     )
-    // })
 
     //to get total number of goals
     // const fuckeverything = goals.length
@@ -58,7 +65,27 @@ export default class extends Component {
     //lol then need to get completed goals also lol WHY DIDNT I USE DATABASE unless i set it into another state property in APP and every time i click complete goal it adds to that count and that's the one i use  woohoo
 
     return (
-        <Box>Hi</Box>
+        <Box
+        direction="row"
+        pad="medium"
+        gap="medium"
+        >
+            {/* active goal container */}
+            <Box>
+                <Text>
+                active goals:
+                {this.getActiveGoals(this.props.goals)}
+                </Text>
+
+                <Text>
+                overdue goals:
+                {this.getOverdueGoals(this.props.goals)}
+                </Text>
+
+            </Box>
+            {/* active goal container */}
+        </Box>
+
     );
   }
 }
